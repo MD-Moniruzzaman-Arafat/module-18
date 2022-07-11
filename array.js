a889b16c1c342a8884800027683089b42ac5484a
MD Moniruzzaman Arafat <arafatmonir091@gmail.com>
2022-07-11 20:59:27 +0600

    array comparison condition nested condition logical operator

---
+++ array.js
@@ -0,0 +1,50 @@
+// var arr = ['arafat', 'salman', 'rakib', 'shajahan'];
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[1])
+// console.log(arr[1] = 'korim');
+// console.log(arr)
+// console.log(arr.indexOf('rofiq'))
+// arr.push('ayaman');
+// arr.pop();
+// console.log(arr)
+
+var number1 = 10;
+var number2 = 5;
+// console.log(number1 == number2)
+// console.log(number1 != number2)
+// console.log(number1 > number2)
+// console.log(number1 < number2)
+// console.log(number1 >= number2)
+// console.log(number1 <= number2)
+
+// if (number1 > number2) {
+//     console.log(number1 + ' is large nubmer')
+// }
+
+// if (number1 == 10 && number2 > 1) {
+//     console.log('say hi!')
+// }
+// if (number1 == 10 || number2 < 1) {
+//     console.log('hi!')
+// }
+
+// if (number1 == 0) {
+//     console.log(number1)
+// } else {
+//     console.log('no')
+// }
+
+if (number1 == 1) {
+    console.log(number1)
+} else if (number2 == 5) {
+    console.log(number2)
+} else {
+    console.log('no')
+}
+
+
+
+
+
+
